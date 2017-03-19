b4479a18c536f337ab04202d43946861fcf36a81
nonoprad <nonoprad@free.fr>
2017-03-19 13:36:03 +0100

    Ajout du comportement Etape on scrolling

--- src/etapes/etapes.module.js
+++ src/etapes/etapes.module.js
@@ -11,25 +11,66 @@ etapeModule.directive('etapes', function(){
 etapeModule.controller('etapesController', function($scope){
 
 
+    $scope.boutons =[
+        { id : 1, libelle : 'Etape 1', selected : true },
+        { id : 2, libelle : 'Etape 2', selected : false },
+        { id : 3, libelle : 'Etape 3', selected : false },
+        { id : 4, libelle : 'Etape 4', selected : false },
+        { id : 5, libelle : 'Etape 5', selected : false }
+    ]
+
 
     window.onscroll = angular.bind(this, function() {
         $scope.showEtapes = true;
+        angular.forEach($scope.boutons,function(bouton){
+            var offsetFromElement = elmYPosition(bouton.id) - currentYPosition();
+            bouton.selected =  !(offsetFromElement < -300 || offsetFromElement >= 300);
+        });
+
+
         setTimeout(function(){
+
+
             $scope.showEtapes = false;
             $scope.$digest();
         },3800);
+
+
         $scope.$digest();
     });
 
-
-    function changePrimaryButton(eID){
-        $scope.groups.forEach(g => g.currentClass='btn');
-        $scope.groups[eID].currentClass= "btn btn-primary";
-
+    let elmYPosition = function(eID) {
+        var elm = document.getElementById("pannel_"+eID);
+        var y = elm.offsetTop;
+        var node = elm;
+        while (node.offsetParent && node.offsetParent != document.body) {
+            node = node.offsetParent;
+            y += node.offsetTop;
+        } return y;
     }
 
-    $scope.scrollTo = function(eID) {
-        changePrimaryButton(eID);
+
+
+    let currentYPosition = function () {
+        // Firefox, Chrome, Opera, Safari
+        if (self.pageYOffset) return self.pageYOffset;
+        // Internet Explorer 6 - standards mode
+        if (document.documentElement && document.documentElement.scrollTop)
+            return document.documentElement.scrollTop;
+        // Internet Explorer 6, 7 and 8
+        if (document.body.scrollTop) return document.body.scrollTop;
+        return 0;
+    }
+
+
+    $scope.scrollTo = function(bouton) {
+        let eID = bouton.id;
+        bouton.selected = true;
+        angular.forEach($scope.boutons,function(occurence){
+           if (bouton.id != occurence.id){
+               occurence.selected= false;
+           }
+        });
         var startY = currentYPosition();
         var stopY = elmYPosition(eID);
         var distance = stopY > startY ? stopY - startY : startY - stopY;
@@ -52,26 +93,8 @@ etapeModule.controller('etapesController', function($scope){
             leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
         }
 
-        function currentYPosition() {
-            // Firefox, Chrome, Opera, Safari
-            if (self.pageYOffset) return self.pageYOffset;
-            // Internet Explorer 6 - standards mode
-            if (document.documentElement && document.documentElement.scrollTop)
-                return document.documentElement.scrollTop;
-            // Internet Explorer 6, 7 and 8
-            if (document.body.scrollTop) return document.body.scrollTop;
-            return 0;
-        }
 
-        function elmYPosition(eID) {
-            var elm = document.getElementById(eID);
-            var y = elm.offsetTop;
-            var node = elm;
-            while (node.offsetParent && node.offsetParent != document.body) {
-                node = node.offsetParent;
-                y += node.offsetTop;
-            } return y;
-        }
+
 
     };
 
